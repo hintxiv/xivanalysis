950505948047e9928cecc64130fce7ffee82ce2d
yumiyafangirl <65056303+yumiyafangirl@users.noreply.github.com>
2021-12-13 16:10:24 -0500

    add changelog

--- src/parser/jobs/mch/index.tsx
+++ src/parser/jobs/mch/index.tsx
@@ -20,6 +20,11 @@ export const MACHINIST = new Meta({
 	],
 	changelog: [
 		{
+			date: new Date('2021-12-13'),
+			Changes: () => <>Updated the Wildfire module.</>,
+			contributors: [CONTRIBUTORS.HINT],
+		},
+		{
 			date: new Date('2021-12-08'),
 			Changes: () => <>Updated Machinist modules for Endwalker.</>,
 			contributors: [CONTRIBUTORS.HINT],
